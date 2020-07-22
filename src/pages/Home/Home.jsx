45234bfc185400baf9d0938dbe323076d3756037
Faiz Hameed <faizftb@gmail.com>
2020-07-22 22:07:38 +0530

    fixing footer responsiveness'

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./Home.scss";
 import Header from "../../components/Header/Header";
 import ImagePanel from "../../components/ImagePanel/ImagePanel";
 import Footer from "../../components/Footer/Footer";
